3314e44d26633d6602eea9a1444a327a600bd5b2
LautaroEM <lautaro.mongelo@gmail.com>
2023-12-12 21:05:53 -0300

    Service Bar iniciando

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,12 +1,62 @@
-function Services() {
+import React from 'react';
+import { Drawer, List, ListItem, ListItemText } from '@mui/material';
 
+const drawerWidth = 240;
 
-    return (
-      <div>
-        
-      </div>
-    );
-  }
-  
-  export default Services;
-  
+const Services = () => {
+  return (
+    <div style={{ display: 'flex' }}>
+      {/* Contenido principal */}
+      <main style={{ flexGrow: 1, padding: '20px' }}>
+        {/* Tu contenido principal aquí */}
+        <h1>Contenido Principal</h1>
+      </main>
+
+      {/* Menú a la izquierda */}
+      <Drawer
+        variant="permanent"
+        style={{
+          width: drawerWidth,
+          flexShrink: 0,
+        }}
+        anchor="left"
+      >
+        <div style={{ width: drawerWidth }}>
+          <List>
+            {/* Categoría 1 */}
+            <ListItem button>
+              <ListItemText primary="Categoría 1" />
+            </ListItem>
+
+            {/* Opción 1.1 */}
+            <ListItem button>
+              <ListItemText inset primary="Opción 1.1" />
+            </ListItem>
+
+            {/* Opción 1.2 */}
+            <ListItem button>
+              <ListItemText inset primary="Opción 1.2" />
+            </ListItem>
+
+            {/* Categoría 2 */}
+            <ListItem button>
+              <ListItemText primary="Categoría 2" />
+            </ListItem>
+
+            {/* Opción 2.1 */}
+            <ListItem button>
+              <ListItemText inset primary="Opción 2.1" />
+            </ListItem>
+
+            {/* Opción 2.2 */}
+            <ListItem button>
+              <ListItemText inset primary="Opción 2.2" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
+
+export default Services;
