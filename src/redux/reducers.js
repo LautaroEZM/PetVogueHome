196734e9cfe4f29789c44d8d638dd603e427a1ff
LautaroEM <lautaro.mongelo@gmail.com>
2024-01-02 15:57:32 -0300

    actualizacion general

---
+++ src/redux/reducers.js
@@ -0,0 +1,40 @@
+// reducers.js
+import {
+    FETCH_SERVICES_REQUEST,
+    FETCH_SERVICES_SUCCESS,
+    FETCH_SERVICES_FAILURE,
+  } from './actions';
+  
+  const initialState = {
+    services: [],
+    loading: false,
+    error: null,
+  };
+  
+  const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case FETCH_SERVICES_REQUEST:
+        return {
+          ...state,
+          loading: true,
+          error: null,
+        };
+      case FETCH_SERVICES_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          services: action.payload,
+        };
+      case FETCH_SERVICES_FAILURE:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+  
+  export default rootReducer;
+  
