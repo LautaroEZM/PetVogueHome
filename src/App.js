073b42e5eec1409495e277500556807ef4620b7d
LautaroEM <lautaro.mongelo@gmail.com>
2024-01-02 16:35:14 -0300

    creado el createService

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from "./Pages/Home/Home";
 import TopBarMenu from "./Components/topBar";
 import CreatePet from "./Pages/CreatePet/CreatePet";
 import Services from "./Pages/Services/Services";
-
 import MyPets from "./Pages/MyPets/MyPets";
+import CreateService from "./Pages/CreateService/CreateService";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/crearMascota" element={<CreatePet />} />
+            <Route path="/crearServicio" element={<CreateService />} />
             <Route path="/servicios" element={<Services />} />
             <Route path="/MisMascotas" element={<MyPets />} />
             {/* Otras rutas pueden agregarse aquí según sea necesario */}
---
+++ src/Pages/CreateService/CreateService.jsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+import { TextField, Button, Box, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import axios from 'axios';
+
+const categories = [
+  'Consulta',
+  'Cirugias',
+  'Especialidades',
+  'Vacunacion',
+  'Peluqueria',
+  'Baños',
+  'Estetica general',
+];
+
+function CreateService() {
+  const [formData, setFormData] = useState({
+    name: '',
+    description: '',
+    category: '',
+    price: '',
+    image: '',
+    status: 'enabled',
+    petID: null,
+    petType: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('https://petvogue.onrender.com/Services', formData);
+      console.log('Solicitud POST exitosa:', response.data);
+      // Puedes realizar alguna acción adicional después de un envío exitoso
+    } catch (error) {
+      console.error('Error al enviar la solicitud POST:', error.message);
+      // Puedes manejar el error de alguna manera, como mostrar un mensaje al usuario
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, maxWidth: 400, margin: 'auto' }}>
+        <TextField
+          label="Nombre del servicio"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <TextField
+          label="Descripción"
+          name="description"
+          value={formData.description}
+          onChange={handleChange}
+          required
+        />
+        <FormControl>
+          <InputLabel id="select-pet-type">Tipo de mascota</InputLabel>
+          <Select
+            label="Tipo de mascota"
+            labelId="select-pet-type"
+            name="petType"
+            value={formData.petType}
+            onChange={handleChange}
+            required
+          >
+            <MenuItem value="Perro">Perro</MenuItem>
+            <MenuItem value="Gato">Gato</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField
+          label="Precio"
+          name="price"
+          type="number"
+          value={formData.price}
+          onChange={handleChange}
+          required
+        />
+        <TextField
+          label="Imagen (URL)"
+          name="image"
+          value={formData.image}
+          onChange={handleChange}
+          required
+        />
+        <FormControl>
+          <InputLabel id="select-category">Categoría</InputLabel>
+          <Select
+            label="Categoría"
+            labelId="select-category"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            required
+          >
+            {categories.map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Button type="submit" variant="contained" color="primary">
+          Crear Servicio
+        </Button>
+      </Box>
+    </form>
+  );
+}
+
+export default CreateService;
