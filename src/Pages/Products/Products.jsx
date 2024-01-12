9f84019fd321f7579b52c01e3a907f38d619ac88
SEmilianoLopez <emilopez197gmail.com>
2024-01-12 18:43:18 -0300

    Base de carrito(sin actualizar)

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -3,7 +3,9 @@ import styles from './Products.module.css';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { getAllProducts } from '../../redux/actions';
-import { Typography , CardHeader,}  from '@mui/material';
+import { Typography , CardHeader,CardActions}  from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const Products = () => {
   const dispatch = useDispatch();
@@ -31,6 +33,17 @@ const Products = () => {
           <Typography>
                   <strong>Precio:</strong> {product.price}
                 </Typography>
+
+          <CardActions sx={{ justifyContent:"right" }}>
+            <IconButton
+              color="primary"
+              aria-label="add to shopping cart"
+              sx={{fontSize: '13px' }}
+            //  onClick={()=>setSelectedProducts([...selectedProducts, {...product,quantity:1}])}
+            >
+                <AddShoppingCartIcon />
+            </IconButton>
+          </CardActions>      
           
         </div>
       ))}
---
+++ src/Pages/ShopCart/ShopCart.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useLocalStorage } from "../../Hooks/useLocalStorage";
+
+
+export const ShopCart = () =>{
+    const [productsCart, setproductsCart] = useLocalStorage("product", []);
+    const [servicesCart, setServicesCart] = useLocalStorage("service", []);
+    const [total, setTotal] = useState(
+        productsCart.reduce((acc, p) => acc + p.price * p.quantity, 0) +
+            servicesCart.reduce((acc, p) => acc + p.price * p.quantity, 0)
+      );
+      
+
+
+      useEffect(() => {
+        setTotal(
+          productsCart.reduce((acc, p) => acc + p.price * p.quantity, 0) +
+            servicesCart.reduce((acc, p) => acc + p.price * p.quantity, 0)
+        );
+      }, [productsCart, servicesCart]);
+
+
+//       const handleAddToCartWithQuantity = (item, quantity, i) => {
+//         const { serviceID } = item;
+//         if (serviceID) {
+//           const newServices = [...servicesCart];
+//           newServices[i]["quantity"] = parseInt(quantity);
+//           setServicesCart(newServices);
+//         }if(item.quantity > item.stock){
+//             alert('Stock Insuficiente')
+//             item.quantity = item.stock
+          
+//         } else {
+//           const newproducts = [...productsCart];
+//           newproducts[i]["quantity"] = parseInt(quantity);
+//           setproductsCart(newproducts);
+//         }
+//     };
+
+
+//     const handleDeleteFromCart = (prodServ) => {
+//         // Eliminar el producto del carrito
+//         const { serviceID } = prodServ;
+//         if (serviceID) {
+//           const updatedCart = servicesCart.filter(
+//             (item) => item.serviceID !== prodServ.serviceID
+//           );
+    
+//           setServicesCart(updatedCart);
+//         } else {
+          
+//           const updatedCart = productsCart.filter(
+//             (item) => item.productID !== prodServ.productID
+//           );
+//           console.log(updatedCart)
+//           setproductsCart(updatedCart);
+//         }
+//       };
+ }
