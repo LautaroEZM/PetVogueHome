e42d301d670075c818db1f50a699b958f55ce9f4
LautaroEM <lautaro.mongelo@gmail.com>
2024-01-18 02:07:40 -0300

    fix

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -76,6 +76,7 @@ const Products = () => {
     
     dispatch(getProducts(searchText, selectedTypes, sortPrice));
   }, []);
+  
 
   const indexOfLastProduct = currentPage * itemsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - itemsPerPage;
