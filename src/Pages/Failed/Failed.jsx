5d13de7b8624381da7f126b1f69fd330639941f4
Guadalupe99 <galbar.guadalupe@gmail.com>
2024-01-18 12:16:03 -0300

    Failed

---
+++ src/Pages/Failed/Failed.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Failed.module.css';  // Importa el archivo CSS
+
+const Failed = () => {
+  const handleRedirect = () => {
+    // Redireccionar manualmente al hacer clic en el botón
+    window.location.replace('https://petvoguehome-production.up.railway.app');
+  };
+
+  return (
+    <div className="failed-container"> {/* Agrega una clase contenedora */}
+      <p className="failed-message">Su compra no se pudo concretar, inténtelo nuevamente.</p>
+      <button className="retry-button" onClick={handleRedirect}>Volver a la página</button>
+    </div>
+  );
+};
+
+export default Failed;
